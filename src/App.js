106928b88f883967c235555d123b747c3dc89efc
ragunath-br <153410999+ragunath-br@users.noreply.github.com>
2024-06-21 08:48:02 +0530

    IMPORTS AND PASSES FAVOURITE COMPONENT AS PROP The favourite component is passed into the MovieList Component as props.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
+import AddFavourite from './components/AddToFavourites';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
@@ -31,7 +32,7 @@ const App = () => {
 				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
 			</div>
 			<div className='row'>
-        <MovieList movies={movies} />
+        <MovieList movies={movies} favouriteComponent={AddFavourites} />
 			</div>
 		</div>
 	);
